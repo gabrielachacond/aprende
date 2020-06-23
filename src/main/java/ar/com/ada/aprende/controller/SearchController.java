b0e25e8d764bc82c82590714afecfeaa260b7402
gabriela chacon <gabrielachacond@gmail.com>
2020-06-22 23:52:41 -0300

    Todo de las busquedas

---
+++ src/main/java/ar/com/ada/aprende/controller/SearchController.java
@@ -0,0 +1,4 @@
+package ar.com.ada.aprende.controller;
+
+public class SearchController {
+}
---
+++ src/main/java/ar/com/ada/aprende/service/SearchEngineServices.java
@@ -0,0 +1,13 @@
+package ar.com.ada.aprende.service;
+
+public class SearchEngineServices {
+
+    //todo: El motor de búsqueda de cursos debe contemplar los siguientes criterios:
+    //● Todo los curso disponibles (con cupos abiertos)
+    //● Todos los curso por Categoría
+    //● Todos los curso por Empresa
+    //● Todos los curso por Participante en progreso
+    //● Todos los curso por Participante finalizados.
+    //● Todos los curso por Empresa y Categoría
+}
+// todo Al menos una clase de cada capa con UNIT e INTEGRATIONS test
